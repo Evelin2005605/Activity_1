373dcd17f7c91decafe1cbd5b3149fccda19a01a
Evelin2005605 <67381512+Evelin2005605@users.noreply.github.com>
2020-06-25 21:21:21 +0530

    Update file.c

--- file.c
+++ file.c
@@ -2,11 +2,13 @@
 int main()
 {
 int a[10],sum=0,i,n;
+int average; 
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
 sum=sum+a[i];
 }
-printf("Sum is %d",sum);
+average=sum/n;
+printf("Sum is %d and average is %d",sum,average);
 }
