a0636db8b50632a2b16999e80e07adf395794f81
Evelin2005605 <67381512+Evelin2005605@users.noreply.github.com>
2020-06-25 22:04:58 +0530

    Update file.c

--- file.c
+++ file.c
@@ -2,13 +2,21 @@
 int main()
 {
 int a[10],sum=0,i,n;
-int average; 
+int average,max=0; 
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
 sum=sum+a[i];
+if(a[i]>max)
+{
+  max=a[i];
+}
 }
 average=sum/n;
 printf("Sum is %d and average is %d",sum,average);
+printf("\nMaximum number is %d",max);
 }
